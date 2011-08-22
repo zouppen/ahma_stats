b2061fddeafd5eb6b2a404cd77001b76fdba477d
Joel Lehtonen <joel.lehtonen@iki.fi>
2011-08-23 00:47:32 +0300

    Parser eats either date or date & time.

--- TimeQuirks.hs
+++ TimeQuirks.hs
@@ -10,11 +10,13 @@ import System.Locale (defaultTimeLocale)
 -- |Parses date and gives error in case of parse error. Absorbing
 -- Maybe because you never want to continue in case of parse error.
 parseWisely :: TimeZone -> String -> DateTime
-parseWisely tz str =
-  case parsed of
-    Just a -> localTimeToUTC tz a
-    Nothing -> error "Cannot parse date. ISO 8601 format required."
-  where parsed = parseTime defaultTimeLocale "%F" str
+parseWisely tz str = localTimeToUTC tz (best parsedTime parsedDate)
+  where
+    parsedDate = parseTime defaultTimeLocale "%F" str
+    parsedTime = parseTime defaultTimeLocale "%F %k:%M" str
+    best (Just a) _ = a
+    best _ (Just a) = a
+    best _ _ = error "Cannot parse date. (Relaxed) ISO 8601 format required."
 
 -- |Parses numeric timezones.
 zoneParse :: String -> Maybe TimeZone
