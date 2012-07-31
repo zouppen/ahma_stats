0a437bffad4cdc6f3470622c863dd18e661f7b90
Joel Lehtonen <joel.lehtonen@iki.fi>
2012-08-01 02:23:41 +0300

    Hilarious typo bug fix

--- couchapp/validate_doc_update.js
+++ couchapp/validate_doc_update.js
@@ -1,6 +1,6 @@
 function(newDoc, oldDoc, userCtx) {
     function inGroup(name) {
-	return userCtx.roles.indexOf('_admin') !== -1;
+	return userCtx.roles.indexOf(name) !== -1;
     }
 
     if (!(inGroup('_admin') || inGroup('ahma'))) {
